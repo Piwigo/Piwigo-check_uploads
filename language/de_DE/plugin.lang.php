520df7a56fae80399e40725a15b5eb0468d45734
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-29 13:36:31 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang[' (size "%s" available in data cache)'] = '(Größe "%s" im Daten-Cache verfügbar)';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d Dateien sind in der Datenbank aufgeführt, aber nicht im Dateisystem verfügbar';
+$lang['%d files deleted'] = '%d Dateien gelöscht';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d Dateien wiederhergestellt (jetzt bitte Metadaten mit dem Batch Manager synchronisieren, um Breite/Höhe/Dateigröße zu aktualisieren)';
+$lang['%d index.htm files with unexpected checksum'] = '%d index.htm Dateien mit unerwarteter Prüfsumme';
+$lang['%d restorable photos from sizes cache.'] = '%d wiederherstellbare Fotos aus dem Größen-Cache.';
+$lang['%d unexpected files.'] = '%d unerwartete Dateien.';
+$lang['%s cannot be removed'] = '%s kann nicht entfernt werden';
+$lang['%s is not in the database'] = '%s ist nicht in der Datenbank';
+$lang['%s unexpected checksum'] = '%s unerwartete Prüfsumme';
+$lang['Check upload directory'] = 'Upload-Verzeichnis prüfen';
+$lang['Delete them all'] = 'Alle löschen';
+$lang['Prefer restoring from a backup.'] = 'Wiederherstellen von einem Backup bevorzugen.';
+$lang['Quality is lower than original files.'] = 'Die Qualität ist schlechter als bei den Originaldateien.';
+$lang['Restore from sizes cache'] = 'Aus Größen-Cache wiederherstellen';
+$lang['Well done! Everything seems good :-)'] = 'Gut gemacht! Alles scheint gut zu sein :-)';
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'Foto #%u path=%s ist in der Datenbank, nicht im Dateisystem';
