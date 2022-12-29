3ada7edb3df71f1d52960090ae905fef1d23fbb1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-29 16:59:17 +0100

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d index.htm files with unexpected checksum'] = '%d index.htm skrár með óvæntri prófsummu';
+$lang['%d unexpected files.'] = '%d óvæntar skrár.';
+$lang['%s cannot be removed'] = '%s er ekki hægt að fjarlægja';
+$lang['%s is not in the database'] = '%s er ekki í gagnagrunninum';
+$lang['%s unexpected checksum'] = '%s óvænt prófsumma';
+$lang['Check upload directory'] = 'Athugaðu upphleðslumöppuna';
+$lang['Delete them all'] = 'Eyða þeim öllum';
+$lang['Prefer restoring from a backup.'] = 'Veljið að endurheimta úr öryggisafriti.';
+$lang['Quality is lower than original files.'] = 'Gæðin eru minni en upprunalegu skrárnar.';
+$lang['Restore from sizes cache'] = 'Endurheimta úr stærðarskyndiminni';
+$lang['Well done! Everything seems good :-)'] = 'Vel gert! Allt lítur vel út :-)';
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'mynd #%u path=%s er í gagnagrunninum, ekki í skráarkerfinu';
+$lang[' (size "%s" available in data cache)'] = '(stærð "%s" tiltæk í skyndiminni gagna)';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d skrár eru skráðar í gagnagrunninum en ekki tiltækar í skráarkerfinu';
+$lang['%d files deleted'] = '%d skrám eytt';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d skrár endurheimtar (núna vinsamlegast, samstilltu lýsigögn við lotustjórann til að uppfæra breidd/hæð/skráarstærð)';
+$lang['%d restorable photos from sizes cache.'] = '%d endurheimtanlegar myndir úr skyndiminnastærð.';
