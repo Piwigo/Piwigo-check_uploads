7ab45e3bda5110ebbcd88cc8738a74ecc78c07d9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-29 13:36:26 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'foto #%u path=%s staat in de database, niet in het bestandssysteem';
+$lang['Well done! Everything seems good :-)'] = 'Goed gedaan! Alles lijkt goed :-)';
+$lang['Restore from sizes cache'] = 'Herstellen vanuit de cache';
+$lang['Quality is lower than original files.'] = 'De kwaliteit is lager dan de originele bestanden.';
+$lang['Prefer restoring from a backup.'] = 'Geef de voorkeur aan herstel vanaf een back-up.';
+$lang['Delete them all'] = 'Verwijder ze allemaal';
+$lang['Check upload directory'] = 'Uploadmap controleren';
+$lang['%s unexpected checksum'] = '%s onverwachte controlesom';
+$lang['%s is not in the database'] = '%s staat niet in de database';
+$lang['%s cannot be removed'] = '%s kan niet worden verwijderd';
+$lang['%d unexpected files.'] = '%d onverwachte bestanden.';
+$lang['%d restorable photos from sizes cache.'] = '%d herstelbare foto\'s uit de cache.';
+$lang['%d index.htm files with unexpected checksum'] = '%d index.htm bestanden met onverwachte controlesom';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d bestanden hersteld (synchroniseer nu metadata met de Batch Manager om breedte/hoogte/bestandsgrootte bij te werken)';
+$lang['%d files deleted'] = '%d bestanden verwijderd';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d bestanden worden vermeld in de database, maar niet beschikbaar in het bestandssysteem';
+$lang[' (size "%s" available in data cache)'] = 'grootte "%s" beschikbaar in gegevenscache)';
