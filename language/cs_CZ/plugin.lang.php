0c9b3d0c921296a74994f6f7924c36363545c69c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-29 13:36:12 +0100

    Update cs_CZ, thanks to MiPi

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Check upload directory'] = 'Zkontrolovat adresář pro nahrávání';
+$lang['Delete them all'] = 'Smazat vše';
+$lang['Prefer restoring from a backup.'] = 'Preferovat obnovení ze zálohy.';
+$lang['Quality is lower than original files.'] = 'Kvalita je nižší než u původních souborů.';
+$lang['Restore from sizes cache'] = 'Obnovit z mezipaměti velikostí';
+$lang['Well done! Everything seems good :-)'] = 'Výborně! Vše vypadá dobře :-)';
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'fotografie #%u cesta=%s je v databázi, ne v souborovém systému';
+$lang[' (size "%s" available in data cache)'] = ' (v datové mezipaměti je k dispozici velikost "%s")';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d souborů je uvedeno v databázi, ale nejsou k dispozici v souborovém systému';
+$lang['%d files deleted'] = '%d souborů smazáno';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d souborů obnoveno (nyní prosím synchronizujte metadata s Batch Managerem a aktualizujte šířku/výšku/velikost souborů)';
+$lang['%d index.htm files with unexpected checksum'] = '%d souborů index.htm s neočekávaným kontrolním součtem';
+$lang['%d restorable photos from sizes cache.'] = '%d obnovitelných fotografií z mezipaměti velikostí.';
+$lang['%d unexpected files.'] = '%d neočekávaných souborů.';
+$lang['%s cannot be removed'] = '%s nelze odstranit';
+$lang['%s is not in the database'] = '%s není v databázi';
+$lang['%s unexpected checksum'] = '%s neočekávaný kontrolní součet';
