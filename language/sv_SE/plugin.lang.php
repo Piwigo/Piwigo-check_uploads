0e17f93c487613f8e23d438d20fd35d4211e6f2a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-29 13:36:22 +0100

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang[' (size "%s" available in data cache)'] = '(storlek "%s" tillgänglig i datacache';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d filer finns listade i databasen men inte tillgängliga på filsystemet';
+$lang['%d files deleted'] = '%d filer raderade';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d filer återställda (var nu god och synkronisera metadata med batch hanteraren för att uppdatera vidd/höjd/filstorlek)';
+$lang['%d index.htm files with unexpected checksum'] = '%d index.htm filer med oväntad kontrollsumma';
+$lang['%d restorable photos from sizes cache.'] = '%d återställbara foton från storlekscachen.';
+$lang['%d unexpected files.'] = '%d oväntade filer.';
+$lang['%s cannot be removed'] = '%s kan inte tas bort';
+$lang['%s is not in the database'] = '%s finns inte i databasen';
+$lang['%s unexpected checksum'] = '%s oväntad kontrollsumma';
+$lang['Check upload directory'] = 'Kontrollera uppladdningskatalogen';
+$lang['Delete them all'] = 'Radera alla';
+$lang['Prefer restoring from a backup.'] = 'Föredra återställning från en backup.';
+$lang['Quality is lower than original files.'] = 'Kvalitén är lägre än originalfilerna.';
+$lang['Restore from sizes cache'] = 'Återställ från storlekscachen';
+$lang['Well done! Everything seems good :-)'] = 'Bra gjort! Allt verkar bra :-)';
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'foto #%u sökväg=%s finns i databasen, inte på filsystemet';
