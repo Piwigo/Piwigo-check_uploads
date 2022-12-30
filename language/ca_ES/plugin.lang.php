2ed7e228a28889c0e74f350b5627da4ae5a6561e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-30 06:50:39 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang[' (size "%s" available in data cache)'] = '(mida "%s" disponible a la memòria cau de dades)';
+$lang['%d files are listed in the database, but not available in the filesystem'] = 'Hi ha %d fitxers llistats a la base de dades però que no estan disponibles al sistema de fitxers';
+$lang['%d files deleted'] = '%d fitxers suprimits';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d fitxers restaurats (ara heu de sincronitzar les metadades amb el Gestor de Lots per actualitzar les amplades, alçades i mides)';
+$lang['%d index.htm files with unexpected checksum'] = '%d fitxers index.htm amb una suma de comprovació inesperada';
+$lang['%d restorable photos from sizes cache.'] = 'Hi ha %d fotos restaurables des de la memòria cau de mides.';
+$lang['%d unexpected files.'] = '%d fitxers inesperats.';
+$lang['%s cannot be removed'] = '%s no es pot eliminar';
+$lang['%s is not in the database'] = '%s no està a la base de dades';
+$lang['%s unexpected checksum'] = 'Suma de comprovació inesperada a %s';
+$lang['Check upload directory'] = 'Comproveu el directori de càrregues';
+$lang['Delete them all'] = 'Suprimeix-ho tot';
+$lang['Prefer restoring from a backup.'] = 'Prefereixo restaurar-ho des d\'una còpia de seguretat';
+$lang['Quality is lower than original files.'] = 'La qualitat no és tan bona com la dels fitxers originals.';
+$lang['Restore from sizes cache'] = 'Restaura des de la memòria cau de mides';
+$lang['Well done! Everything seems good :-)'] = 'Ben fet! Tot sembla correcte :-)';
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'La foto %u amb camí d\'accés %s hi és a la base de dades però no en al sistema de fitxers';
