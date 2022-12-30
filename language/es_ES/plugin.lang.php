0ab518e797c19dcd069d1e0f9c9be5803334ed66
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-30 06:50:43 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang[' (size "%s" available in data cache)'] = '(tamaño "%s" disponible en la caché de datos)';
+$lang['%d files are listed in the database, but not available in the filesystem'] = 'Hay %d archivos listados en la base de datos pero no disponibles en el sistema de archivos';
+$lang['%d files deleted'] = '%d archivos suprimidos';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d archivos restaurados (ahora es necesario que sincronice los metadatos con el Gestor de Lotes para actualizar las dimensiones y el tamaño)';
+$lang['%d index.htm files with unexpected checksum'] = '%d archivos index.htm con una suma de comprobación inesperada ';
+$lang['%d restorable photos from sizes cache.'] = 'Hay %d fotos restaurables desde la caché de tamaños.';
+$lang['%d unexpected files.'] = '%d archivos inesperados.';
+$lang['%s cannot be removed'] = '%s no se puede eliminar';
+$lang['%s is not in the database'] = '%s no está en la base de datos';
+$lang['%s unexpected checksum'] = 'Suma de comprobación inesperada en %s';
+$lang['Check upload directory'] = 'Compruebe el directorio de subidas';
+$lang['Delete them all'] = 'Suprimirlo todo';
+$lang['Prefer restoring from a backup.'] = 'Mejor restaurarlo desde una copia de seguridad.';
+$lang['Quality is lower than original files.'] = 'La calidad es inferior que la de los archivos originales.';
+$lang['Restore from sizes cache'] = 'Restaurar desde la caché de tamaños';
+$lang['Well done! Everything seems good :-)'] = '¡Bien hecho! Todo parace correcto :-)';
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'La foto nº %u con vía de acceso %s está en la base de datos pero no en el sistema de archivos';
