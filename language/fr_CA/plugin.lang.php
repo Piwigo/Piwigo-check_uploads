f8edda66140cad4f689a12113cf83e44f0bcc9d1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-31 07:03:35 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d fichiers affichés dans la base de données ont disparu du système de fichiers';
+$lang['%d restorable photos from sizes cache.'] = '%d photos récupérables à partir du cache.';
+$lang['Prefer restoring from a backup.'] = 'Le mieux est de récupérer à partir de votre sauvegarde.';
+$lang['Restore from sizes cache'] = 'Récupérer à partir du cache';
