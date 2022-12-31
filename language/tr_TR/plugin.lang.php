7d67858f52241068c2df3541e138d8abd935598f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-31 07:03:31 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'fotoğraf #%u path=%s dosya sisteminde değil, veri tabanında';
+$lang['Well done! Everything seems good :-)'] = 'Harikasınız! Her şey iyi görünüyor :-)';
+$lang['Restore from sizes cache'] = 'Boyut önbelleğinden geri yükle';
+$lang['Quality is lower than original files.'] = 'Kalite orijinal dosyadan düşüktür.';
+$lang['Prefer restoring from a backup.'] = 'Bir yedekten geri yüklemeyi tercih edin.';
+$lang['Delete them all'] = 'Tümünü sil';
+$lang['Check upload directory'] = 'Yükleme dizinini kontrol edin';
+$lang['%s unexpected checksum'] = '%s beklenmeyen kontrol toplamı';
+$lang['%s is not in the database'] = '%s veri tabanında yok';
+$lang['%s cannot be removed'] = '%s kaldırılamaz';
+$lang['%d unexpected files.'] = '%d beklenmeyen dosya.';
+$lang['%d index.htm files with unexpected checksum'] = '%d beklenmeyen kontrol toplamına sahip index.htm dosyası mevcut';
+$lang['%d restorable photos from sizes cache.'] = '%d boyut önbelleğinden geri yüklenebilir fotoğraf.';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d dosya geri yüklendi (şimdi lütfen genişlik/yükseklik/dosya boyutunu güncellemek için meta verileri Toplu İş Yöneticisi ile senkronize edin)';
+$lang['%d files deleted'] = '%d dosya silindi';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d dosya veri tabanında listeleniyor, ancak dosya sisteminde mevcut değil';
+$lang[' (size "%s" available in data cache)'] = '(veri tampon belleğinde "%s" kullanılabilir alan var)';
