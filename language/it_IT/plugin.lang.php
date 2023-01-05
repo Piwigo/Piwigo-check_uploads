5f240708850890dadcd33df1a8e8b3547d64c484
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-05 12:23:10 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d index.htm files with unexpected checksum'] = '%d index.htm file con checksum inatteso.';
+$lang['%d restorable photos from sizes cache.'] = '%d fotografie ripristinabile dalla memoria cache.';
+$lang['%d unexpected files.'] = '%d file inattesi.';
+$lang['%s cannot be removed'] = '%s non può essere rimosso.';
+$lang['%s is not in the database'] = '%s non è presente nel database.';
+$lang['%s unexpected checksum'] = '%s checksum inatteso.';
+$lang['Check upload directory'] = 'Controlla la cartella di caricamento.';
+$lang['Delete them all'] = 'Cancellali tutti.';
+$lang['Prefer restoring from a backup.'] = 'Preferisci il ripristino da un backup.';
+$lang['Quality is lower than original files.'] = 'Qualità inferiore rispetto al file originale.';
+$lang['Restore from sizes cache'] = 'Ripristina dalla memoria cache.';
+$lang['Well done! Everything seems good :-)'] = 'Ben fatto! tutto sembra a posto :-)';
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'La fotografia #%u path=%s è nel database, ma non nel filesystem.';
+$lang[' (size "%s" available in data cache)'] = '(size "%s" disponibile nella cache dei dati).';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d file sono elencati nel database, ma non disponibili nel filesystem.';
+$lang['%d files deleted'] = '%d file eliminati.';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d file ripristinati (ora per favore sincronizza i metadati mediante il Gestore dei Lotti per aggiornare larghezza/altezza/dimensione)';
