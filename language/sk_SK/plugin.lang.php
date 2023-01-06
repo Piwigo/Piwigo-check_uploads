be64f4fab0e39c1715dfc4973fe2c27d0803f920
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-06 09:27:16 +0100

    Update sk_SK, thanks to dodo

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'fotografia #%u path=%s je v databáze, nie v súborovom systéme';
+$lang['Well done! Everything seems good :-)'] = 'Výborne! Všetko sa zdá byť dobré :-)';
+$lang['Restore from sizes cache'] = 'Obnoviť z vyrovnávacej pamäte veľkostí';
+$lang['Quality is lower than original files.'] = 'Kvalita je nižšia ako u pôvodných súborov.';
+$lang['Prefer restoring from a backup.'] = 'Uprednostňujte obnovenie zo zálohy.';
+$lang['Delete them all'] = 'Vymažte ich všetky';
+$lang['Check upload directory'] = 'Skontrolujte adresár pre nahrávanie';
+$lang['%s unexpected checksum'] = '%s neočakávaný kontrolný súčet';
+$lang['%s is not in the database'] = '%s nie je v databáze';
+$lang['%s cannot be removed'] = '%s nie je možné odstrániť';
+$lang['%d unexpected files.'] = '%d neočakávaných súborov.';
+$lang['%d restorable photos from sizes cache.'] = '%d obnoviteľných fotografií z vyrovnávacej pamäte veľkostí.';
+$lang['%d index.htm files with unexpected checksum'] = '%d súbory index.htm s neočakávaným kontrolným súčtom';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d súborov obnovených (teraz prosím synchronizujte metadáta so správcom dávok, aby ste aktualizovali šírku/výšku/veľkosť súboru)';
+$lang['%d files deleted'] = '%d odstránených súborov';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d súborov je uvedených v databáze, ale nie sú dostupné v súborovom systéme';
+$lang[' (size "%s" available in data cache)'] = '(veľkosť "%s" je k dispozícii vo vyrovnávacej pamäti údajov)';
