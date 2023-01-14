8b75877f30c00b67551ad25a7859e499703868d7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-14 15:31:08 +0100

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d unexpected files.'] = '%d niespodziewanych plików.';
+$lang[' (size "%s" available in data cache)'] = '(rozmiar "%s" dostępny w buforze danych)';
+$lang['%d index.htm files with unexpected checksum'] = '%d plików index.htm z niespodziewaną sumą kontrolną';
+$lang['%d restorable photos from sizes cache.'] = '%d zdjęć możliwych do odtworzenia z buforu rozmiarów.';
+$lang['Restore from sizes cache'] = 'Odtwórz z buforu rozmiarów';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d plików odtworzonych (proszę zsynchronizować teraz metadane przy użyciu Batch Managera aby uaktualnić szerokość/wysokość/wielkość pliku)';
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'zdjęcie #%u ścieżka=%s jest w bazie, ale nie w systemie plików';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d plików jest zarejestrowanych w bazie, ale niedostępnych w systemie plików';
+$lang['%d files deleted'] = '%d plików usuniętych';
+$lang['%s cannot be removed'] = '%s nie może być skasowany';
+$lang['%s is not in the database'] = '%s nie ma w bazie danych';
+$lang['%s unexpected checksum'] = 'niespodziewana suma kontrolna dla %s';
+$lang['Check upload directory'] = 'Sprawdź folder wysyłania';
+$lang['Delete them all'] = 'Skasuj je wszystkie';
+$lang['Prefer restoring from a backup.'] = 'Preferuj odtwarzanie z kopii zapasowej.';
+$lang['Quality is lower than original files.'] = 'Jakość jest niższa niż plików oryginalnych.';
+$lang['Well done! Everything seems good :-)'] = 'Dobra robota! Wszystko wygląda w porządku :-)';
