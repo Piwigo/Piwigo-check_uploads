19c0c0910bd8f4dd1d9030280c2cc7cca25491e5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:23:59 +0200

    Update ru_RU, thanks to rurio

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang[' (size "%s" available in data cache)'] = '(размер "%s" доступен в кэше данных)';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d файлов перечислены в базе данных, но недоступны в файловой системе';
+$lang['%d files deleted'] = '%d файлов удалено';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = 'Восстановлено %d файлов (теперь, пожалуйста, синхронизируйте метаданные с пакетным менеджером, чтобы обновить ширину/высоту/размер файла)';
+$lang['%d index.htm files with unexpected checksum'] = '%d файлов index.htm с неожиданной контрольной суммой';
+$lang['%d restorable photos from sizes cache.'] = '%d фотографий, которые можно восстановить из кэша размеров.';
+$lang['%d unexpected files.'] = '%d неожиданных файлов.';
+$lang['%s cannot be removed'] = '%s не может быть удален';
+$lang['%s is not in the database'] = '%s нет в базе данных';
+$lang['%s unexpected checksum'] = '%s непредвиденная контрольная сумма';
+$lang['Check upload directory'] = 'Проверить каталог загрузки';
+$lang['Delete them all'] = 'Удалить все';
+$lang['Prefer restoring from a backup.'] = 'Лучше восстановить из резервной копии.';
+$lang['Quality is lower than original files.'] = 'Качество ниже оригинальных файлов.';
+$lang['Restore from sizes cache'] = 'Восстановить из кеша размеров';
+$lang['Well done! Everything seems good :-)'] = 'Отличная работа! Вроде все хорошо :-)';
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'photo #%u path=%s находится в базе данных, но не в файловой системе';
