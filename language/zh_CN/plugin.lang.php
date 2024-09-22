d673fd6401204d884ba63e659f18109a2ed0c34d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-22 07:22:53 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang[' (size "%s" available in data cache)'] = '（数据缓存中可用的大小为“%s”）';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d 个文件已在数据库中列出，但在文件系统中不可用';
+$lang['%d files deleted'] = '已删除 %d 个文件';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '已恢复 %d 个文件（现在请将元数据与批处理管理器同步以更新宽度/高度/文件大小）';
+$lang['%d index.htm files with unexpected checksum'] = '%d 个 index.htm 文件具有意外的校验和';
+$lang['%d restorable photos from sizes cache.'] = '%d 张可从缓存中恢复的照片。';
+$lang['%d unexpected files.'] = '%d 个意外文件。';
+$lang['%s cannot be removed'] = '%s 无法删除';
+$lang['%s is not in the database'] = '%s 不在数据库中';
+$lang['%s unexpected checksum'] = '%s 意外校验和';
+$lang['Check upload directory'] = '检查上传目录';
+$lang['Delete them all'] = '全部删除';
+$lang['Prefer restoring from a backup.'] = '最好从备份中恢复。';
+$lang['Quality is lower than original files.'] = '质量低于原始文件。';
+$lang['Restore from sizes cache'] = '从缓存中恢复';
+$lang['Well done! Everything seems good :-)'] = '做得好！一切看起来都很好 :-)';
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = '照片 #%u path=%s 在数据库中，而不是在文件系统中';
