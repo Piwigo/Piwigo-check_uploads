bfea9b98fe0d82b3dccb59388263795efc0b2a25
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:06:26 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang[' (size "%s" available in data cache)'] = '(גודל "%s" זמין במטמון הנתונים)';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d קבצים מופיעים במסד הנתונים, אך אינם זמינים במערכת הקבצים';
+$lang['%d files deleted'] = '%d קבצים נמחקו';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d קבצים שוחזרו (עכשיו, אנא סנכרן את המטה-נתונים עם מנהל האצווה כדי לעדכן רוחב/גובה/גודל קובץ)';
+$lang['%d index.htm files with unexpected checksum'] = '%d קבצי index.htm עם בדיקת קוד מחדל לא צפויה';
+$lang['%d restorable photos from sizes cache.'] = '%d תמונות שניתן לשחזר ממטמון הגדלים';
+$lang['%d unexpected files.'] = '%d קבצים לא צפויים';
+$lang['%s cannot be removed'] = '%s לא ניתן להסרה';
+$lang['%s is not in the database'] = '%s אינו נמצא במסד הנתונים';
+$lang['%s unexpected checksum'] = '%s בדיקת קוד מחדל לא צפויה';
+$lang['Check upload directory'] = 'בדוק את תיקיית ההעלאה';
+$lang['Delete them all'] = 'מחק את כל הקבצים';
+$lang['Prefer restoring from a backup.'] = 'מועדף לשחזר מגיבוי';
+$lang['Quality is lower than original files.'] = 'איכות נמוכה מהקבצים המקוריים';
+$lang['Restore from sizes cache'] = 'שחזר ממטמון הגדלים';
+$lang['Well done! Everything seems good :-)'] = 'כל הכבוד! הכל נראה תקין :-)';
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'תמונה #%u נתיב=%s נמצאת במסד הנתונים, לא במערכת הקבצים';
