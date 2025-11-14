482e7306342600a272783db7fe8a240776456d5d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-11-14 14:25:06 +0100

    Update el_GR, thanks to dimitri

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang[' (size "%s" available in data cache)'] = '(το μέγεθος "%s" είναι διαθέσιμο στην προσωρινή μνήμη δεδομένων)';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d αρχεία είναι καταγραμμένα στη βάση δεδομένων, αλλά δεν είναι διαθέσιμα στο σύστημα αρχείων';
+$lang['%d files deleted'] = 'Διαγράφηκαν %d αρχεία';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = 'Επαναφέρθηκαν %d αρχεία (τώρα συγχρονίστε τα μεταδεδομένα με τον Διαχειριστή Δεσμών Ενεργειών για να ανανεώσετε τα πλάτος/ύψος/μέγεθος αρχείου)';
+$lang['%d index.htm files with unexpected checksum'] = '%d αρχεία καταλόγου με μη αναμενόμενο άθροισμα ελέγχου';
+$lang['%d restorable photos from sizes cache.'] = '%d φωτογραφίες επαναφέρσιμες από την προσωρινή μνήμη μεγεθών.';
+$lang['%d unexpected files.'] = '%d μη αναμενόμενα αρχεία';
+$lang['%s cannot be removed'] = 'Το %s δεν μπορεί να διαγραφεί';
+$lang['%s is not in the database'] = 'Το %s δεν είναι στη βάση δεδομένων';
+$lang['%s unexpected checksum'] = '%s μη αναμενόμενο άθροισμα ελέγχου';
+$lang['Check upload directory'] = 'Ελέγξτε τον κατάλογο μεταφόρτωσης';
+$lang['Delete them all'] = 'Διαγράψτε τα όλα';
+$lang['Prefer restoring from a backup.'] = 'Προτίμηση για επαναφορά από ένα αντίγραφο.';
+$lang['Quality is lower than original files.'] = 'Η ποιότητα είναι χαμηλότερη από τα αυθεντικά αρχεία.';
+$lang['Restore from sizes cache'] = 'Επανέφερε από την προσωρινή μνήμη μεγεθών';
+$lang['Well done! Everything seems good :-)'] = 'Μπράβο! Όλα φαίνονται εντάξει!';
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = 'η φωτογραφία #%u διαδρομής=%s είναι στη βάση δεδομένων, όχι στο σύστημα αρχείων';
