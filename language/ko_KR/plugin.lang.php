6c0f5d7e47a0200a2ce6d53a8ac92cec5524551c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-02 11:45:03 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['photo #%u path=%s is in the database, not in the filesystem'] = '사진 #%u path=%s는 데이터베이스에 있지만 파일 시스템에는 없음';
+$lang['Well done! Everything seems good :-)'] = '잘하셨습니다! 모든 것이 정상인 것 같습니다 :-)';
+$lang['Restore from sizes cache'] = '사이즈 캐시에서 복원';
+$lang['Quality is lower than original files.'] = '원본 파일보다 품질이 낮습니다.';
+$lang['Prefer restoring from a backup.'] = '백업에서 복원하는 것을 선호합니다.';
+$lang['Delete them all'] = '전체 삭제';
+$lang['Check upload directory'] = '업로드 디렉터리 확인';
+$lang['%s unexpected checksum'] = '%s 예상치 못한 체크섬';
+$lang['%s is not in the database'] = '%s는 데이터베이스에 없음';
+$lang['%s cannot be removed'] = '%s는 제거할 수 없음';
+$lang['%d unexpected files.'] = '%d개의 예상치 못한 파일.';
+$lang['%d restorable photos from sizes cache.'] = '사이즈 캐시에서 %d개 복원 가능한 사진.';
+$lang['%d index.htm files with unexpected checksum'] = '예기치 않은 체크섬이 있는 %d개 index.htm 파일';
+$lang['%d files restored (now please, synchronize metadata with the Batch Manager to update width/height/filesize)'] = '%d개 파일이 복원되었습니다 (이제 Batch Manager와 메타데이터를 동기화하여 너비/높이/파일 크기를 업데이트하십시오)';
+$lang['%d files deleted'] = '%d개 파일이 삭제되었습니다';
+$lang['%d files are listed in the database, but not available in the filesystem'] = '%d개 파일이 데이터베이스에 등록되었으나 파일 시스템에서 사용 불가합니다';
+$lang[' (size "%s" available in data cache)'] = ' (데이터 캐시에 %s 크기 사용 가능)';
